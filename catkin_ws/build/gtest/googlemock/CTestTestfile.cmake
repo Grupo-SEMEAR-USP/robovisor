2bb216669348915233709c47174f06e08f07a563
KLC8-DFCM <diegofleurycm@gmail.com>
2022-08-06 14:58:34 -0300

    Retiradas pastas build e devel (só dar catkin_make que restaura) adicionado pacote do controlador do drive diferencial

--- catkin_ws/build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/diego-fleury/Documents/SEMEAR/Projetos/Robovisor/robovisor/catkin_ws/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
