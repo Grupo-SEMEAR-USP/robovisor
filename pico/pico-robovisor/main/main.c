ebe74b5a1d7c9d5162bd259db8e862dd26a0b8f8
toschilt <lt.toschi@gmail.com>
2022-08-18 19:40:04 -0300

    Debugging target velocity

--- pico/pico-robovisor/main/main.c
+++ pico/pico-robovisor/main/main.c
@@ -84,9 +84,15 @@ void read_velocity_commands(float *velocity)
                 printf("[%d] %d\n", i, serialBuffer[i]);
             }
 
-            printf("[RECEIVING] velocity_l[0] = %x, velocity_l[1] = %x, velocity_l[2] = %x, velocity_l[3] = %x\n", velocity_l[0], velocity_l[1], velocity_l[2], velocity_l[3]);
-            printf("[RECEIVING] velocity_r[0] = %x, velocity_r[1] = %x, velocity_r[2] = %x, velocity_r[3] = %x\n", velocity_r[0], velocity_r[1], velocity_r[2], velocity_r[3]);
-            printf("[RECEIVING] velocity[LEFT] = %.2f, velocity[RIGHT] = %.2f\n", velocity[LEFT], velocity[RIGHT]);
+            if(last_velocity_target[LEFT] != velocity[LEFT] || last_velocity_target[RIGHT] != velocity[RIGHT])
+            {
+                last_velocity_target[LEFT] = velocity[LEFT];
+                last_velocity_target[RIGHT] = velocity[RIGHT];
+
+                printf("[RECEIVING] velocity_l[0] = %x, velocity_l[1] = %x, velocity_l[2] = %x, velocity_l[3] = %x\n", velocity_l[0], velocity_l[1], velocity_l[2], velocity_l[3]);
+                printf("[RECEIVING] velocity_r[0] = %x, velocity_r[1] = %x, velocity_r[2] = %x, velocity_r[3] = %x\n", velocity_r[0], velocity_r[1], velocity_r[2], velocity_r[3]);
+                printf("[RECEIVING] velocity[LEFT] = %.2f, velocity[RIGHT] = %.2f\n", velocity[LEFT], velocity[RIGHT]);
+            }
         }
     }
 }
